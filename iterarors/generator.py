9520d97d8f8af26eb89605677de6c4350f5497cd
Alexander Belyavsky <andeithms@yandex.ru>
2021-02-15 14:54:22 +0300

    edit

--- iterarors/generator.py
+++ iterarors/generator.py
@@ -5,7 +5,7 @@ def generate_file_md5(file_path):
 
     with open(file_path, "rb") as f:
         file_hash = hashlib.md5()
-        while chunk := f.read(8192):
+        while chunk := f.readline(8192):
             yield chunk
             file_hash.update(chunk)
             print(file_hash.digest())
