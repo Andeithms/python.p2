1ef2aa2e6fd9f42b604c791d3586dde4ae3e1583
Alexander Belyavsky <andeithms@yandex.ru>
2021-03-26 12:25:20 +0300

    v 1.0

--- diplom2/Test.py
+++ diplom2/Test.py
@@ -1,22 +1,23 @@
 import unittest
 import Database as DB
 from vk_bot import MyBot
-from unittest.mock import patch
 import vk_api
+from data.config import login, password
 
 
 class MyTest(unittest.TestCase):
 
     def setUp(self):
-        self.login, self.password = '', ''
+        self.user_id = 'windgood'
+        self.login, self.password = login, password
         self.vk_session = vk_api.VkApi(self.login, self.password)
         try:
             self.vk_session.auth(token_only=True)
         except vk_api.AuthError as error_msg:
             print(error_msg)
 
-        self.doc = [(362185124,), (350195378,), (272507299,), (174682711,), (9950888,),
-                    (133290766,), (205399292,), (176711574,), (156384242,), (327596850,)]
+        self.doc = [362185124, 350195378, 272507299, 174682711, 9950888,
+                    133290766, 205399292, 176711574, 156384242, 327596850]
         self.dict = {'https://vk.com/id362185124': [
             'https://sun9-68.userapi.com/impf/c639319/v639319124/22c8/KkZAhg4wkL4.jpg?size=720x720&quality=96&sign=d1e'
             '95d43ef9ca8fb4fab826c6869d369&c_uniq_tag=moYGfZxkr6bueG_kldXPkrU3hBIohCALVawEBeCpJ-s&type=album',
@@ -25,16 +26,21 @@ class MyTest(unittest.TestCase):
             'https://sun9-38.userapi.com/impf/c837225/v837225124/2602/YxVLHWBpg28.jpg?size=617x617&quality=96&sign='
             '8e7ce974c4b07e6c54ccba2c87d362f9&c_uniq_tag=6wAjT-PiKbkDHK_nyv4PwWLm-r7LtkszRd_4U4D8K2o&type=album']}
 
-    @patch('builtins.input', side_effect=['windgood', 'да'])
-    def test_collect_info(self, user_inp):
-        self.assertEqual(MyBot.collect_info(self), ('windgood', [56, '1998', 2]))
+    def test_collect_info(self):
+        vk = self.vk_session.get_api()
+        response = vk.users.get(user_ids=self.user_id, fields='sex, bdate, city')
+        self.assertEqual(MyBot.get_sex(self, response), 2)
+        self.assertEqual(MyBot.get_age(self, response), '1998')
+        self.assertEqual(MyBot.get_town(self, response), 56)
 
     def test_get_data(self):
-        name = 'windgood'
-        self.assertEqual(DB.get_data(name), self.doc)
+        self.assertEqual(DB.get_data(self.user_id), self.doc)
 
     def test_get_photo(self):
         self.assertEqual(MyBot.get_photo(self, [362185124]), self.dict)
+
+    def history(self):  # чтобы избежать ошибки при вызове записи в БД в test_get_photo
+        pass
 
     def tearDown(self):
         pass
